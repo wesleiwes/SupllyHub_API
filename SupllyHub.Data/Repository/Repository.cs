aa2c864960a87280db22fff3cb58a69a4dd3d7a1
wesleiwes <wesleiralves@gmail.com>
2025-11-10 23:34:04 -0300

    feat: Implementa repositório abstrato

---
+++ SupllyHub.Data/Repository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using SupllyHub.Business.Interfaces.Repository;
+using Microsoft.EntityFrameworkCore;
+using SupllyHub.Business.Models;
+using SupllyHub.Data.Context;
+using System.Linq.Expressions;
+
+namespace SupllyHub.Data.Repository;
+public abstract class Repository<TEntity>(MyDbContext context) : IRepository<TEntity> where TEntity : Entity, new()
+{
+    protected readonly MyDbContext context = context;
+    protected readonly DbSet<TEntity> DbSet = context.Set<TEntity>();
+
+    public virtual Task Add(TEntity entity)
+    {
+        DbSet.Add(entity);
+        return SaveChanges();
+    }
+
+    public virtual async Task Delete(Guid id)
+    {
+        DbSet.Remove(new TEntity { Id = id});
+        await SaveChanges();
+    }
+
+    public virtual async Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate) =>
+        await DbSet.AsNoTracking().Where(predicate).ToListAsync();
+
+    public virtual async Task<IEnumerable<TEntity>> GetAll() =>
+        await DbSet.ToListAsync();
+
+    public virtual async Task<TEntity> GetById(Guid id) => await DbSet.FindAsync(id);
+
+    public virtual async Task Update(TEntity entity)
+    {
+        DbSet.Update(entity);
+        await SaveChanges();
+    }
+
+    public async Task<int> SaveChanges() => 
+        await context.SaveChangesAsync();
+
+    public void Dispose() => context?.Dispose();
+}
