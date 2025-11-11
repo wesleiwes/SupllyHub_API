33e06e52f75dcf6b9327a044cc7b3c4a8e1e2427
wesleiwes <wesleiralves@gmail.com>
2025-11-10 23:42:17 -0300

    refactor: edita propriedade

--- SupllyHub.Business/Interfaces/Repository/IRepository.cs
+++ SupllyHub.Business/Interfaces/Repository/IRepository.cs
@@ -6,7 +6,7 @@ public interface IRepository<TEntity> : IDisposable where TEntity : Entity
 {
     Task Add(TEntity entity);
     Task<TEntity> GetById(Guid id);
-    Task<TEntity> GetAll(string name);
+    Task<IEnumerable<TEntity>> GetAll();
     Task Update(TEntity entity);
     Task Delete(Guid id);
     Task<IEnumerable<TEntity>> Get(Expression<Func<TEntity, bool>> predicate);
