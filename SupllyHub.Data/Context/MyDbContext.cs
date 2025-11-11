c72aefdbd93eb9e23e1ec397b02ab2380722cb67
wesleiwes <wesleiralves@gmail.com>
2025-11-10 23:40:38 -0300

    feat: Adiciona context EF

---
+++ SupllyHub.Data/Context/MyDbContext.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SupllyHub.Business.Models;
+
+namespace SupllyHub.Data.Context;
+public class MyDbContext : DbContext
+{
+    public MyDbContext(DbContextOptions options) : base(options)
+    {
+        ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        ChangeTracker.AutoDetectChangesEnabled = false;
+    }
+
+    DbSet<Supplier> Suppliers { get; set; }
+    DbSet<Product> Products { get; set; }
+    DbSet<Address> Addresses { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        foreach (var property in modelBuilder.Model.GetEntityTypes()
+                                                   .SelectMany(e => e.GetProperties()
+                                                   .Where(p => p.ClrType == typeof(string))))
+        {
+            property.SetColumnType("varchar(100)");
+        }
+
+        modelBuilder.ApplyConfigurationsFromAssembly(typeof(MyDbContext).Assembly);
+
+        foreach (var relationship in modelBuilder.Model.GetEntityTypes().SelectMany(e => e.GetForeignKeys()))
+        {
+            relationship.DeleteBehavior = DeleteBehavior.ClientSetNull;
+        }
+
+        base.OnModelCreating(modelBuilder);
+    }
+
+    public override Task<int> SaveChangesAsync(CancellationToken cancellationToken = new CancellationToken())
+    {
+        foreach (var entry in ChangeTracker.Entries().Where(entry => entry.Entity.GetType().GetProperty("DataRegistration") != null))
+        {
+            if (entry.State == EntityState.Added)
+            {
+                entry.Property("DataRegistration").CurrentValue = DateTime.Now;
+            }
+
+            if (entry.State == EntityState.Modified)
+            {
+                entry.Property("DataRegistration").IsModified = false;
+            }
+        }
+
+        return base.SaveChangesAsync(cancellationToken);
+    }
+}
