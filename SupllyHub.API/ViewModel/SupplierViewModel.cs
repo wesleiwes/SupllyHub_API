91a7d9a677a2e03de2b1d218778ca2b2bab1a36a
wesleiwes <wesleiralves@gmail.com>
2025-11-10 23:37:16 -0300

    feat: implementa view model

---
+++ SupllyHub.API/ViewModel/SupplierViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupllyHub.API.ViewModel;
+
+public class SupplierViewModel
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(100, ErrorMessage ="O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+    public string? Document { get; set; }
+    public int SupplierType { get; set; }
+    public AddressViewModel? Address { get; set; }
+    public bool Active { get; set; }
+    public IEnumerable<ProductViewModel>? Products { get; set; }
+}
