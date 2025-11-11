37bc481ec50fd52ba544b353c3af5712942887cb
wesleiwes <wesleiralves@gmail.com>
2025-11-10 23:55:08 -0300

    feat: Adiciona repositório do endereço

--- SupllyHub.Data/Repository/AddressRepository.cs
+++ SupllyHub.Data/Repository/AddressRepository.cs
@@ -1,12 +1,11 @@
-﻿using SupllyHub.Business.Interfaces.Repository;
+﻿using Microsoft.EntityFrameworkCore;
+using SupllyHub.Business.Interfaces.Repository;
 using SupllyHub.Business.Models;
 using SupllyHub.Data.Context;
 
 namespace SupllyHub.Data.Repository;
 public class AddressRepository(MyDbContext context) : Repository<Address>(context), IAddressRepository
 {
-    public Task<Address> GetAddressBySupplier(Guid fornecedorId)
-    {
-        throw new NotImplementedException();
-    }
+    public Task<Address> GetAddressBySupplier(Guid supplierId) =>
+        context.Addresses.AsNoTracking().FirstOrDefaultAsync(s => s.SupplierId == supplierId);
 }
