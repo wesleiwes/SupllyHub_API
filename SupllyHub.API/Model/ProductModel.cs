08698b086321cb6e41847cc47a81b23b8243c0cc
wesleiwes <wesleiralves@gmail.com>
2025-11-22 11:42:33 -0300

    feat - adiciona models e validações

---
+++ SupllyHub.API/Model/ProductModel.cs
@@ -0,0 +1,35 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SupllyHub.API.Model;
+
+public class ProductModel
+{
+    [Key]
+    public Guid Id { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public Guid SupllierId { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(200, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+    public string? Name { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    [StringLength(1000, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+    public string? Description { get; set; }
+
+    public string? ImageUpload {  get; set; }
+
+    public string? Image { get; set; }
+
+    [Required(ErrorMessage = "O campo {0} é obrigatório")]
+    public decimal Value { get; set; }
+
+    [ScaffoldColumn(false)]
+    public DateTime RegistrationDate { get; set; }
+
+    public bool Active { get; set; }
+
+    [ScaffoldColumn(false)]
+    public string? SupplierName { get; set; }
+}
