5a7d662d5b4dbe66930ffc76324169ee44793024
wesleiwes <wesleiralves@gmail.com>
2025-11-22 11:40:53 -0300

    feat - Adiciona mapeador de entidades

---
+++ SupllyHub.API/Configuration/AutoMapperConfig.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using SupllyHub.API.Model;
+using SupllyHub.Business.Models;
+
+namespace SupllyHub.API.Configuration;
+
+public class AutoMapperConfig : Profile
+{
+    public AutoMapperConfig()
+    {
+        CreateMap<Supplier, SupplierModel>().ReverseMap();
+        CreateMap<Address, AddressModel>().ReverseMap();
+        CreateMap<Product, ProductModel>().ReverseMap();
+    }
+}
