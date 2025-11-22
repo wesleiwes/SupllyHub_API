c9966130034c0a2084bfc5a3cd93bb8a39689b52
wesleiwes <wesleiralves@gmail.com>
2025-11-22 11:41:21 -0300

    feat - adiciona injeção de dependencia

---
+++ SupllyHub.API/Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,18 @@
+﻿using SupllyHub.Business.Interfaces.Repository;
+using SupllyHub.Data.Context;
+using SupllyHub.Data.Repository;
+
+namespace SupllyHub.API.Configuration;
+
+public static class DependencyInjectionConfig
+{
+    public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+    {
+        services.AddScoped<MyDbContext>();
+        services.AddScoped<ISupplierRepository, SupplierRepository>();
+        services.AddScoped<IAddressRepository, AddressRepository>();
+        services.AddScoped<IProductRepository, ProductRepository>();
+
+        return services;
+    }
+}
